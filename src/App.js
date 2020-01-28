abc0097c6e7b4b0730210eebf7c3be39d04bd151
EvgheniCernev <evghenic@readcast.net>
2020-01-29 01:12:31 +0200

    app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Create from './Create';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <Create />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Create.jsx
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Form, Checkbox, Input, Button } from 'semantic-ui-react';
+import soundfileON from './audio/on.mp3';
+import soundfileOFF from './audio/off.mp3';
+import './Create.css';
+
+class Create extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      interval: 0,
+      selectedTime: 'min',
+      status: 'on',
+      background: '#0a7617'
+    };
+    this.audioON = new Audio(soundfileON);
+    this.audioOFF = new Audio(soundfileOFF);
+  }
+
+  calcTime = () => {
+    if (this.state.selectedTime === 'ho') {
+      return this.state.interval * 3600000;
+    }
+    if (this.state.selectedTime === 'min') {
+      return this.state.interval * 60000;
+    }
+    if (this.state.selectedTime === 'sec') {
+      return this.state.interval * 1000;
+    }
+  };
+  componentWillMount() {
+    axios
+      .get('http://localhost:3001/status')
+      .then(response => console.log(response));
+  }
+
+  handleSubmit = status => e => {
+    e.preventDefault();
+    setTimeout(() => {
+      axios
+        .post(`http://localhost:3001/${status}`, { ledStatus: status })
+        .then(res => {
+          status === 'on' ? this.audioON.play() : this.audioOFF.play();
+          this.setState({
+            status: status === 'on' ? 'off' : 'on',
+            background: status === 'on' ? '#ed3330' : '#0a7617'
+          });
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }, this.calcTime());
+  };
+
+  handleChange = event => {
+    this.setState({ interval: event.target.value });
+  };
+
+  handleOptionChange = event => {
+    this.setState({
+      selectedTime: event.target.value
+    });
+  };
+
+  render() {
+    console.log(this.state.status);
+    return (
+      <div className='wrapper'>
+        <div className='select-time'>
+          <Form>
+            <Form.Field>
+              <Checkbox
+                radio
+                label='House'
+                name='checkboxRadioGroup'
+                value='ho'
+                checked={this.state.selectedTime === 'ho'}
+                onChange={this.handleOptionChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Checkbox
+                radio
+                label='Minutes'
+                name='checkboxRadioGroup'
+                value='min'
+                checked={this.state.selectedTime === 'min'}
+                onChange={this.handleOptionChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <Checkbox
+                radio
+                label='Seconds'
+                name='checkboxRadioGroup'
+                value='sec'
+                checked={this.state.selectedTime === 'sec'}
+                onChange={this.handleOptionChange}
+              />
+            </Form.Field>
+          </Form>
+          <div>
+            <Input
+              label={{ basic: true, content: this.state.selectedTime }}
+              className='enter-time'
+              labelPosition='right'
+              placeholder='Enter time...'
+            />
+            <Button
+              style={{ background: this.state.background }}
+              color={this.state.background}
+              onClick={this.handleSubmit(this.state.status)}
+            >
+              {this.state.status}
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Create;
